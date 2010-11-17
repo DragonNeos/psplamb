eb82241406da995bc616da76956a16df08e57e81
marcin.ploska@gmail.com <marcin.ploska@gmail.com@cbcbbe76-952c-6284-d426-79555ba45a39>
2010-11-17 23:28:45 +0000

    New tool and first version of working model exporter plugin.

--- trunk/plugin vr 2/hook_display.c
+++ trunk/plugin vr 2/hook_display.c
@@ -68,6 +68,8 @@ int currentDList_restarted = 0;
 //end dispay list stuff
 
 //vertex stuff
+unsigned int vertexAddres = 0;
+unsigned int vertexAddresTemp = 0;
 unsigned int vertexBase = 0;
 unsigned int vertexPntr = 0;
 unsigned int base = 0;
@@ -99,6 +101,32 @@ int normalOffset = 0;
 int positionOffset = 0;
 int oneSize = 0;
 
+typedef struct TextNorVertex
+{
+	float u,v;
+	float nx,ny,nz;
+	float x,y,z;
+};
+
+typedef struct 
+{
+	float u,v;
+	float x,y,z;
+}TexVertex;
+
+typedef struct NorVertex
+{
+	float nx,ny,nz;
+	float x,y,z;
+};
+
+typedef struct SkinTexVertex
+{
+	float w[8];
+	float u,v;
+	float x,y,z;
+};
+
 //texture info
 int texture_height[8] = { 0, 0, 0, 0, 0, 0, 0, 0 };
 int texture_width[8] = { 0, 0, 0, 0, 0, 0, 0, 0 };
@@ -109,9 +137,19 @@ int texture_storage_mode = 0;
 //end vertex stuff
 unsigned int addressMask = 0x3FFFFFFF;
 
+//bones stuff
+int boneMatrixIndex = 0;
+float bone_uploaded_matrix[8][4 * 3];
+
+
 
 int can_parse = 0;
+
+int argif = 0;
+float fargument = 0.0f;
+
 int primitiveCounter = 0;
+SceUID file;
 
 float getFloat(unsigned int data){ data<<=8; return *(float*)(&data);} 
 
@@ -138,11 +176,6 @@ void ExecuteCommand(int command,int argument)
 {
 	switch(command)
 	{
-		
-		
-
-
-		
 		case 0x01://VADR
 		{
 			vertexBase = ( base | argument) + baseOffset;
@@ -183,9 +216,381 @@ void ExecuteCommand(int command,int argument)
 			int numberOfVertex = argument & 0xFFFF;
 			int type = ((argument >> 16) & 0x7);
 
-			char text[70];
-			sprintf(text,"%d %d\n",numberOfVertex,type);
-			debuglog(text);
+			//char text[70];
+			//sprintf(text,"%d %d\n",numberOfVertex,type);
+			//debuglog(text);
+			
+			
+			//start reading vertext info
+			//only skinned and not 2d vertices
+			if(iweight != 0 && itexture != 0 && itransform2D == 0)
+			{
+			
+				if(file >= 0)
+				{
+					//how many vertex
+					sceIoWrite(file, &numberOfVertex, sizeof(int));
+					//vertex type
+					sceIoWrite(file, &type, sizeof(int));
+				}
+				
+				//iterate on every vertex
+				int vertCounter = 0;
+				for(vertCounter = 0; vertCounter < numberOfVertex; vertCounter++ )
+				{
+					//we must get memory adres of each vertex
+					//but we must check if vertex indexind is used
+					int indexcount = vertCounter;
+					
+					//temp data for every vertex
+					float boneWeights[8];
+					float tu,tv;
+					float nx,ny,nz;
+					float px,py,pz;
+					
+					//if there is indexing
+					if(indexPntr != 0 && iindex != 0)
+					{
+						//yes there is indexing
+						unsigned int vertexOffset = indexPntr + (indexcount * iindex);
+						
+						switch(iindex)
+						{
+							case 1: 
+							{
+								unsigned char uc8 = 0;
+								memcpy(&uc8,(unsigned char *)vertexOffset,1);
+								indexcount = (unsigned int)uc8;
+							}
+							break;
+							case 2:
+							{
+								vertexOffset = (vertexOffset + 1) & ~1;
+								short sh16 = 0;
+								memcpy(&sh16,(unsigned char *)vertexOffset,2);
+								indexcount = (unsigned int)sh16;
+							}
+							break;
+							case 4:
+							{
+								vertexOffset = (vertexOffset + 3) & ~3;
+								unsigned int in32 = 0;
+								memcpy(&in32,(unsigned char *)vertexOffset,4);
+								indexcount = in32;
+							}
+							break;
+						}
+					}
+					
+					//set vertex addres in memory
+					vertexAddres = vertexBase + indexcount * vertexSize;
+					//for morphing vertices
+					vertexAddresTemp = vertexAddres;
+					
+					//weight
+					if(iweight != 0)
+					{
+						int weightNumber = 0;
+						for(weightNumber = 0; weightNumber < iskinningWeightCount; weightNumber++)
+						{
+							switch(iweight)
+							{
+								case 1:
+								{
+									unsigned char uc8 = 0;
+									memcpy(&uc8,(unsigned char *)vertexAddres,1);
+									boneWeights[weightNumber] = (float)uc8 /127.0f;
+									vertexAddres++;
+								}
+								break;
+
+								case 2:
+								{
+									vertexAddres = (vertexAddres + 1) & ~1;
+									short s16 = 0;
+									memcpy(&s16,(unsigned char *)vertexAddres,2);
+									boneWeights[weightNumber] = (float)s16 / 32767.0f;
+									vertexAddres+=2;
+								}
+								break;
+
+								case 3:
+								{
+									vertexAddres = (vertexAddres + 3) & ~3;
+									float f32 = 0.0f;
+									memcpy(&f32,(unsigned char *)vertexAddres,4);
+									boneWeights[weightNumber] = f32;
+									vertexAddres+=4;
+								}
+								break;
+							}
+						}
+					}
+					
+					//texture uv
+					//texture
+					if(itexture != 0)
+					{
+						switch(itexture)
+						{
+							case 1:
+							{
+								char c8 = 0;
+								
+								memcpy(&c8,(unsigned char *)vertexAddres,1);
+								tu = (float)c8;
+								vertexAddres++;
+								
+								memcpy(&c8,(unsigned char *)vertexAddres,1);
+								tv = (float)c8;
+								vertexAddres++;
+								
+								tu/=127.0f;
+								tv/=127.0f;
+							}
+							break;
+							case 2:
+							{
+								short s16 = 0;
+								vertexAddres = (vertexAddres + 1) & ~1;
+								
+								memcpy(&s16,(unsigned char *)vertexAddres,2);
+								tu = (float)s16;
+								vertexAddres+=2;
+								
+								memcpy(&s16,(unsigned char *)vertexAddres,2);
+								tv = (float)s16;
+								vertexAddres+=2;
+								
+								tu/=32767.0f;
+								tv/=32767.0f;
+							}
+							break;
+							case 3:
+							{
+								vertexAddres = (vertexAddres + 3) & ~3;
+								
+								memcpy(&tu,(unsigned char *)vertexAddres,4);
+								memcpy(&tv,(unsigned char *)vertexAddres,4);
+							}
+						}
+					}
+					
+					//color
+					if(icolor != 0)
+					{
+						switch(icolor)
+						{
+							case 1:
+							case 2:
+							case 3:
+							{
+								vertexAddres+=1;
+							}
+							break;
+							case 4:
+							case 5:
+							case 6:
+							{
+								vertexAddres = (vertexAddres + 1) & ~1;
+								vertexAddres+=2;
+							}
+							break;
+							case 7:
+							{
+								vertexAddres = (vertexAddres + 3) & ~3;
+								vertexAddres+=4;
+							}
+							break;
+						}
+					}
+					
+					//normal
+					if(inormal != 0)
+					{
+						switch(inormal)
+						{
+							case 1:
+							{
+								vertexAddres+=3;
+							}
+							break;
+							case 2:
+							{
+								vertexAddres = (vertexAddres + 1) & ~1;
+								vertexAddres+=6;
+							}
+							break;
+							case 3:
+							{
+								vertexAddres = (vertexAddres + 3) & ~3;
+								vertexAddres+=12;
+							}
+							break;
+						}
+					}
+					
+					//position
+					if(iposition != 0)
+					{
+						switch(iposition)
+						{
+							case 1:
+							{
+								char c8 = 0;
+								unsigned char uc8 = 0;
+								
+								// X and Y are signed 8 bit, Z is unsigned 8 bit
+								memcpy(&c8,(unsigned char *)vertexAddres,1);
+								px = (float)c8;
+								vertexAddres++;
+								
+								memcpy(&c8,(unsigned char *)vertexAddres,1);
+								py = (float)c8;
+								vertexAddres++;
+								
+								memcpy(&uc8,(unsigned char *)vertexAddres,1);
+								pz = (float)uc8;
+								vertexAddres++;
+								
+								px /= 127.0f;
+								py /= 127.0f;
+								pz /= 127.0f;
+							}
+							break;
+							
+							case 2:
+							{
+								short s16 = 0;
+								vertexAddres = (vertexAddres + 1) & ~1;
+
+								// X and Y are signed 16 bit, Z is unsigned 16 bit
+								memcpy(&s16,(unsigned char *)vertexAddres,2);
+								px = (float)s16;
+								vertexAddres+=2;
+								
+								memcpy(&s16,(unsigned char *)vertexAddres,2);
+								py = (float)s16;
+								vertexAddres+=2;
+								
+								memcpy(&s16,(unsigned char *)vertexAddres,2);
+								pz = (float)s16;
+								vertexAddres+=2;
+								
+								px /= 32767.0f;
+								py /= 32767.0f;
+								pz /= 32767.0f;
+
+							}
+							break;
+							
+							case 3:
+							{
+								vertexAddres = (vertexAddres + 3) & ~3;
+
+								memcpy(&px,(unsigned char *)vertexAddres,4);
+								vertexAddres+=4;
+								memcpy(&py,(unsigned char *)vertexAddres,4);
+								vertexAddres+=4;
+								memcpy(&pz,(unsigned char *)vertexAddres,4);
+								vertexAddres+=4;
+							}
+							break;
+						}
+						
+						//skinning
+						if (iweight != 0)
+						{
+							//temp values
+							float sx = 0, sy = 0, sz = 0;
+							
+							int weightNumber = 0;
+							for(weightNumber = 0; weightNumber < iskinningWeightCount; weightNumber++)
+							{
+								if (boneWeights[weightNumber] != 0.0f)
+								{
+									sx += (	px * bone_uploaded_matrix[weightNumber][0]
+									+ 		py * bone_uploaded_matrix[weightNumber][3]
+									+ 		pz * bone_uploaded_matrix[weightNumber][6]
+									+            bone_uploaded_matrix[weightNumber][9]) * boneWeights[weightNumber];
+
+									sy += (	px * bone_uploaded_matrix[weightNumber][1]
+									+ 		py * bone_uploaded_matrix[weightNumber][4]
+									+ 		pz * bone_uploaded_matrix[weightNumber][7]
+									+            bone_uploaded_matrix[weightNumber][10]) * boneWeights[weightNumber];
+
+									sz += (	px * bone_uploaded_matrix[weightNumber][2]
+									+ 		py * bone_uploaded_matrix[weightNumber][5]
+									+ 		pz * bone_uploaded_matrix[weightNumber][8]
+									+            bone_uploaded_matrix[weightNumber][11]) * boneWeights[weightNumber];
+								}
+							}
+							
+							//swap temp variables
+							px = sx;
+							py = sy;
+							pz = sz;							
+						}
+						
+					}
+
+					if(file > 0)
+					{
+						//save vertex to file
+						TexVertex vertex;
+						vertex.u = tu;
+						vertex.v = tv;
+						vertex.x = px;
+						vertex.y = py;
+						vertex.z = pz;
+						
+						sceIoWrite(file, &vertex, sizeof(TexVertex));
+					}
+				}
+			
+			}
+			
+			// VADDR is updated after vertex rendering.
+			// Some games rely on this and don't reload VADDR between 2 PRIM calls.
+			//vertex_adr = vinfo.getAddress(mem, numberOfVertex);
+			int indexcount = numberOfVertex;
+			if(indexPntr != 0 && iindex != 0)
+			{
+				unsigned int addr = indexPntr + indexcount * iindex;
+
+				switch(iindex)
+				{
+				case 1: 
+					{
+						unsigned char uc8 = 0;
+						memcpy(&uc8,(unsigned char *)addr,1);
+						indexcount = (unsigned int)uc8;
+					}
+					break;
+				case 2:
+					{
+						addr = (addr + 1) & ~1;
+						short sh16 = 0;
+						memcpy(&sh16,(unsigned char *)addr,2);
+						indexcount = (unsigned int)sh16;
+					}
+					break;
+				case 4:
+					{
+						addr = (addr + 3) & ~3;
+						unsigned int in32 = 0;
+						memcpy(&in32,(unsigned char *)addr,4);
+						indexcount = in32;
+					}
+					break;
+				}
+				
+				indexPntr += iindex * numberOfVertex;
+
+			}else
+			{
+				vertexBase = vertexBase + indexcount * vertexSize;
+			}
 			
 			//debugCommand(command,argument);
 		}
@@ -266,9 +671,9 @@ void ExecuteCommand(int command,int argument)
 			vertexSize = (vertexSize + alignmentSize - 1) & ~(alignmentSize - 1);
 			oneSize = (oneSize + alignmentSize - 1) & ~(alignmentSize - 1);
 
-			char text[100];
-			sprintf(text,"t:%d c:%d n:%d p:%d w:%d i:%d sk:%d mo:%d tr:%d\n",itexture,icolor,inormal,iposition,iweight,iindex,iskinningWeightCount,imorphingVertexCount,itransform2D);
-			debuglog(text);
+			//char text[100];
+			//sprintf(text,"t:%d c:%d n:%d p:%d w:%d i:%d sk:%d mo:%d tr:%d\n",itexture,icolor,inormal,iposition,iweight,iindex,iskinningWeightCount,imorphingVertexCount,itransform2D);
+			//debuglog(text);
 			
 			//debugCommand(command,argument);
 		}
@@ -330,9 +735,9 @@ void ExecuteCommand(int command,int argument)
 		{
 			currentDList_ended = 1;
 			
-			char text[100];
-			sprintf(text,"End \n");
-			debuglog(text);
+			//char text[100];
+			//sprintf(text,"End \n");
+			//debuglog(text);
 
 		}
 		break;
@@ -341,9 +746,32 @@ void ExecuteCommand(int command,int argument)
 		{
 			currentDList_finished = 1;
 			
-			char text[100];
-			sprintf(text,"Finish \n");
-			debuglog(text);
+			//char text[100];
+			//sprintf(text,"Finish \n");
+			//debuglog(text);
+		}
+		break;
+		
+		//bone matrix support
+		case 0x2A:
+		{
+			boneMatrixIndex = argument;
+		}
+		break;
+
+		case 0x2B:
+		{
+			argif = argument << 8;
+			memcpy(&fargument,&argif,4);
+
+			int matrixIndex  = boneMatrixIndex / 12;
+			int elementIndex = boneMatrixIndex % 12;
+
+			if (matrixIndex < 8)
+			{
+				bone_uploaded_matrix[matrixIndex][elementIndex] = fargument;
+				boneMatrixIndex++;
+			}
 		}
 		break;
 		
@@ -367,9 +795,9 @@ void ExecuteCommand(int command,int argument)
 			texture_height[level] = 1 << height_exp2;
             texture_width[level] = 1 << width_exp2;
 			
-			char text[100];
-			sprintf(text,"Tex level: %d size: %d x %d\n",level,texture_width[level],texture_height[level]);
-			debuglog(text);
+			//char text[100];
+			//sprintf(text,"Tex level: %d size: %d x %d\n",level,texture_width[level],texture_height[level]);
+			//debuglog(text);
 		}
 		break;
 		
@@ -378,9 +806,9 @@ void ExecuteCommand(int command,int argument)
 		{
 			texture_storage_mode = argument & 0xF; // Lower four bits.
 			
-			char text[100];
-			sprintf(text,"Tex format: %d \n",texture_storage_mode);
-			debuglog(text);
+			//char text[100];
+			//sprintf(text,"Tex format: %d \n",texture_storage_mode);
+			//debuglog(text);
 		}
 		break;
 		
@@ -404,6 +832,11 @@ void RipData()
 		char text[70];
 		sprintf(text,"addres1: %x addres2: %x\n",currentDList_addr,currentDList_stall_addr);
 		debuglog(text);
+		
+		//open file
+		char modelFilename[50];
+		sprintf(modelFilename,"ms0:/models/model%d.3d",primitiveCounter);
+		file = sceIoOpen(modelFilename, PSP_O_CREAT | PSP_O_WRONLY, 0777);
 		
 		while (!listHasEnded)
 		{
@@ -432,6 +865,13 @@ void RipData()
 				ExecuteCommand(command,argument);
 			}		
 		}
+		
+		if(file >= 0)
+		{
+			sceIoClose(file);
+			primitiveCounter++;
+		}
+		
 		
 		currentDList_finished = 0;
 		currentDList_ended = 0;
